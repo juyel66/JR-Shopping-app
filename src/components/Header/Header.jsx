e922091f1dbb8e74687e5d777f61ab02a727a352
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2024-05-07 17:30:58 +0600

    jr shopping app

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Header.css';
+import logo from '../../images/Logo.svg';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <nav className='header'>
+            {/* <img src={logo} alt="" /> */}
+            <h1 className='jr'>JR Shopping</h1>
+            <div>
+                <Link to="/">Shop</Link>
+                <Link to="/orders">Orders</Link>
+                <Link to="/inventory">Inventory</Link>
+                <Link to="/login">Login</Link>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
